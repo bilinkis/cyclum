ca623d1536f2d02581e6d91fad71f55e924c49d8
joelsobolmark <joelsobolmark@gmail.com>
2016-04-14 13:13:30 +0000

    clone

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -131,24 +131,12 @@
     </div>
 
     <footer class="page-footer">
-        <div class="footer-row container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Acerca de Cyclum</h5>
-                    <p class="grey-text text-lighten-4">Cyclum se puede lograr solo con sangre, sudor y lagrimas asi que los obligamos a donarnos $1.000.000 de dolares. Gracias por colaborar.</p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                    <h5 class="white-text">Contacto</h5>
-                    <ul>
-                        <li><a class="grey-text text-lighten-3" target="_blank" href="http://www.twitter.com/Cyclum.IO">Twitter</a></li>
-                        <li><a class="grey-text text-lighten-3" target="_blank" href="http://www.facebook.com/Cyclum.IO">Facebook</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
         <div class="footer-copyright">
             <div class="container">
-                © 2016 Brand New Paraguas Inc.<a class="grey-text text-lighten-4 right scroll-animation" href="#nav-bar">Volver a arriba</a>
+                © 2016 Cyclum.
+                <a class="grey-text text-lighten-3" target="_blank" href="http://www.twitter.com/Cyclum.IO">Twitter</a>
+                <a class="grey-text text-lighten-3" target="_blank" href="http://www.facebook.com/Cyclum.IO">Facebook</a>
+                <a class="grey-text text-lighten-4 right scroll-animation" href="#nav-bar">Volver a arriba</a>
             </div>
         </div>
     </footer>
